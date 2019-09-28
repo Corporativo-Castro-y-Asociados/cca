6484f2af77cbd234ab38a9ca30c49d1c37336a05
Alejandro Camarillo <alejandro.camarillo@wizeline.com>
2019-09-28 01:35:34 -0500

    chore: HTML Pages

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-export default () => (
-  <div>
-    Hola Mundo.{' '}
-    <Link href='/about' as={process.env.BACKEND_URL + '/about'}>
-      <a>Acerca</a>
-    </Link>
-  </div>
-)
